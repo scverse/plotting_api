fee5ea66ce1283fc144fa2b596bb0c3dc76f4b99
Philipp A. <flying-sheep@web.de>
2024-02-26 13:00:31 +0100

    package structure

--- src/scverse_plotting_api/__init__.py
+++ src/scverse_plotting_api/__init__.py
@@ -1,9 +1,17 @@
-from anndata import AnnData
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
 import matplotlib.pyplot as plt
 import scanpy as sc
 
+if TYPE_CHECKING:
+    from anndata import AnnData
 
-def plot_umap(adata: AnnData, color: str, *, ax: plt.Axes | plt.SubplotSpec | None = None) -> plt.Figure | None:
+
+def plot_umap(
+    adata: AnnData, color: str, *, ax: plt.Axes | plt.SubplotSpec | None = None
+) -> plt.Figure | None:
     # prologue
     ax_was_provided = ax is not None
     gridspec_params = {"nrows": 1, "ncols": 2, "width_ratios": (0.7, 0.1)}
@@ -16,18 +24,25 @@ def plot_umap(adata: AnnData, color: str, *, ax: plt.Axes | plt.SubplotSpec | No
     elif isinstance(ax, plt.Axes):
         fig = ax.get_figure()
         if ax.get_subplotspec() is None:
-            raise ValueError("Ax object must be based on a gridspec, e.g. generated using plt.subplots()")
+            msg = (
+                "Ax object must be based on a gridspec, "
+                "e.g. generated using plt.subplots()"
+            )
+            raise ValueError(msg)
         else:
             gs = ax.get_subplotspec().subgridspec(**gridspec_params)
             ax.remove()
     else:
-        raise ValueError("If specified, ax must be an Axes or SubplotSpec object")
+        msg = "If specified, ax must be an Axes or SubplotSpec object"
+        raise ValueError(msg)
 
     # plotting
     main_ax = fig.add_subplot(gs[0, 0])
     cbar_ax = fig.add_subplot(gs[0, 1])
     x, y = zip(*adata.obsm["X_umap"])
-    scatter = main_ax.scatter(x, y, s=3, c=sc.get.obs_df(adata, color).values, cmap="viridis")
+    scatter = main_ax.scatter(
+        x, y, s=3, c=sc.get.obs_df(adata, color).values, cmap="viridis"
+    )
     fig.colorbar(scatter, cax=cbar_ax)
 
     # epilogue
