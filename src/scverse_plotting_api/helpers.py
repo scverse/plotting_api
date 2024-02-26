a0258d96c8cbe8d53983284aa87b2c4c9d516b8d
Philipp A. <flying-sheep@web.de>
2024-02-26 16:08:59 +0100

    almost

--- src/scverse_plotting_api/helpers.py
+++ src/scverse_plotting_api/helpers.py
@@ -4,7 +4,7 @@ import inspect
 from contextlib import contextmanager
 from functools import wraps
 from itertools import islice
-from typing import TYPE_CHECKING, Concatenate, ParamSpec, TypeVar
+from typing import TYPE_CHECKING, TypedDict, overload
 
 from matplotlib import get_backend
 from matplotlib.axes import Axes
@@ -12,26 +12,63 @@ from matplotlib.figure import Figure
 from matplotlib.gridspec import GridSpec, SubplotSpec
 
 if TYPE_CHECKING:
-    from collections.abc import Callable, Generator
+    from collections.abc import Generator
+    from typing import Generic, ParamSpec, Protocol
+
+    from matplotlib.gridspec import GridSpecFromSubplotSpec
 
     P = ParamSpec("P")
-    R = TypeVar("R")
+
+    class PlottingImpl(Protocol, Generic[P]):
+        def __call__(
+            self,
+            *args: P.args,
+            fig: Figure,
+            gs: GridSpec | GridSpecFromSubplotSpec,
+            **kwds: P.kwargs,
+        ) -> None:
+            ...
+
+    class PlottingAPI(Protocol, Generic[P]):
+        @overload
+        def __call__(self, *args: P.args, ax: None = None, **kwds: P.kwargs) -> Figure:
+            ...
+
+        @overload
+        def __call__(
+            self, *args: P.args, ax: Axes | SubplotSpec, **kwds: P.kwargs
+        ) -> None:
+            ...
 
 
-# TODO: make ax a kwarg. Depends on:
-# https://github.com/python/typing/issues/1009
-def plot_decorator(
-    f: Callable[Concatenate[Figure, GridSpec, P], R],
-) -> Callable[Concatenate[Axes | SubplotSpec, P], R]:
+def plot_decorator(f: PlottingImpl[P]) -> PlottingAPI[P]:
+    @overload
+    def wrapper(
+        *args: P.args,
+        ax: None = None,
+        **kwargs: P.kwargs,
+    ) -> Figure:
+        ...
+
+    @overload
+    def wrapper(
+        *args: P.args,
+        ax: Axes | SubplotSpec,
+        **kwargs: P.kwargs,
+    ) -> None:
+        ...
+
     @wraps(f)
-    def wrapper(ax: Axes, *args: P.args, **kwargs: P.kwargs) -> R:
+    def wrapper(
+        *args: P.args, ax: Axes | SubplotSpec | None = None, **kwargs: P.kwargs
+    ) -> Figure | None:
         with plot_context(ax) as (fig, gs):
-            rv = f(fig, gs, *args, **kwargs)
+            rv = f(*args, fig=fig, gs=gs, **kwargs)  # type: ignore[func-returns-value]
             if rv is not None:
-                msg = f"{f.__name__}’ definition should not return anything"
+                name = getattr(f, "__name__", "function")
+                msg = f"{name}’ definition should not return anything"
                 raise TypeError(msg)
-        if ax is None:
-            return fig
+        return fig if ax is None else None
 
     ax_param = inspect.Parameter(
         "ax", inspect.Parameter.POSITIONAL_ONLY, annotation=Axes | SubplotSpec
@@ -40,7 +77,7 @@ def plot_decorator(
     del wrapper.__annotations__["gs"]
     wrapper.__annotations__["ax"] = ax_param.annotation
 
-    wrapper.__signature__ = (sig := inspect.signature(f)).replace(
+    wrapper.__signature__ = (sig := inspect.signature(f)).replace(  # type: ignore[attr-defined]
         parameters=[
             ax_param,
             *islice(sig.parameters.values(), 2, None),
@@ -50,13 +87,21 @@ def plot_decorator(
     return wrapper
 
 
+class GSParams(TypedDict):
+    nrows: int
+    ncols: int
+    width_ratios: tuple[float, float]
+
+
 @contextmanager
 def plot_context(
     ax: Axes | SubplotSpec | None = None,
-) -> Generator[tuple[Figure, GridSpec], None, None]:
+) -> Generator[tuple[Figure, GridSpec | GridSpecFromSubplotSpec], None, None]:
     # prologue
     ax_was_provided = ax is not None
-    gridspec_params = {"nrows": 1, "ncols": 2, "width_ratios": (0.7, 0.1)}
+    gridspec_params = GSParams(nrows=1, ncols=2, width_ratios=(0.7, 0.1))
+    fig: Figure | None
+    gs: GridSpec | GridSpecFromSubplotSpec
     if not ax_was_provided:
         fig = Figure(layout="tight")
         gs = GridSpec(figure=fig, **gridspec_params)
@@ -67,14 +112,14 @@ def plot_context(
     elif isinstance(ax, Axes):
         fig = ax.get_figure()
         assert fig is not None
-        if ax.get_subplotspec() is None:
+        if (subplotspec := ax.get_subplotspec()) is None:
             msg = (
                 "Ax object must be based on a gridspec, "
                 "e.g. generated using plt.subplots()"
             )
             raise ValueError(msg)
         else:
-            gs = ax.get_subplotspec().subgridspec(**gridspec_params)
+            gs = subplotspec.subgridspec(**gridspec_params)
             ax.remove()
     else:
         msg = "If specified, ax must be an Axes or SubplotSpec object"
